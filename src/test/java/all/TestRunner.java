11235b852f9a7f14db4eba5a1353578550c45392
jana <jana.r.a.droubi@gmail.com>
2025-05-16 15:41:00 +0300

    jana

--- src/test/java/all/TestRunner.java
+++ src/test/java/all/TestRunner.java
@@ -5,7 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType;
 import org.junit.runner.RunWith;
 
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/resources/Uses_Cases"},
